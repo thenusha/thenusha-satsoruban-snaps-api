dd65752b0754515afd6a7401260b7f6955ebf8ad
thenusha satsoruban <thenusha.s@gmail.com>
2025-02-27 14:47:31 -0500

    added get and listen w port

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 import express from 'express';
+
+const app = express();
+app.get('/', function(req, res){
+    res.send('Welome to /');
+});
+
+app.listen(8080, function(){
+    console.log('Hello world');
+});
